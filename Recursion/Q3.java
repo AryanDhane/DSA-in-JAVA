61f89e1721554a4f8672afd6d807c14ee151bef6
AryanDhane <dhanearyan150074@gmail.com>
2026-08-05 15:53:54 +0530

    ..

---
+++ Recursion/Q3.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class Q3 {
+
+    public static void printFact(int n, int fact){
+        if(n == 0){
+            System.out.println(fact);
+            return;
+        }
+        fact *= n;
+        printFact(n-1, fact);
+    }
+    public static void main(String args[]){
+        printFact(5, 1);
+    }
+}
+
+
+// ouput
+
+// 120
